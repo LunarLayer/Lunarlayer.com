286ec1f6317385d73755623fb55e1875004cc8f6
TomSteinmeier <113900695+LunarLayer@users.noreply.github.com>
2023-01-23 01:13:45 +0100

    Cors
    Attempt #4

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 const PORT = process.env.PORT || 8800;
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://lunarlayer.com/"); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', '*');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
